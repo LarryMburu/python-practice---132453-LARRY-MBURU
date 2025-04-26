3bb7adfa2215cfbf1c11e2ae0a190db6bf5de307
LarryMburu <168400774+LarryMburu@users.noreply.github.com>
2025-04-26 23:23:37 +0300

    Create reverse_string_function.py

---
+++ reverse_string_function.py
@@ -0,0 +1,11 @@
+
+def reverse_string(s):
+    reversed_str = ''
+    for char in s:
+        reversed_str = char + reversed_str
+    return reversed_str
+
+print(reverse_string("hello"))
+
+//this functions has the capability to reverse a string (write a word backwards) from its original form
+//for example, for this one its answer is 'olleh'
