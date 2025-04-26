1c6ec394ddaa4171654b887b1c71a4ada31ae74c
LarryMburu <168400774+LarryMburu@users.noreply.github.com>
2025-04-26 17:30:54 +0300

    Update sum_list.py

--- sum_list.py
+++ sum_list.py
@@ -1,1 +1,10 @@
+def sum_list(numbers):
+    total = 0
+    for num in numbers:
+        total += num
+    return total
 
+print(sum_list([1, 2, 3, 4, 5]))
+
+//so what this function does is that it takes a list of numbers and then adds all of them using the function call sum_list(numbers) which has already beed fdefined before being called
+//which means 1+2+3+4+5 = 15, but now itll be done using the method
