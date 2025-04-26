2e3c6aeeedb0726ebd9170154963562660b2c13b
LarryMburu <168400774+LarryMburu@users.noreply.github.com>
2025-04-26 23:33:20 +0300

    Update sum_of_a_number.py

--- sum_of_a_number.py
+++ sum_of_a_number.py
@@ -1,1 +1,7 @@
+def sum_of_digits(num):
+    return sum(int(digit) for digit in str(num))
 
+print(sum_of_digits(1234))  
+
+//This function basically separates all the digits that make mup the  number and calculates the sum of digits of the number, 
+// eg 1234 will become 1+2+3+4 which = 10
